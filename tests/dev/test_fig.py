05644542d3324b1dc1e7ab300a648ac26d051af2
yo-aka-gene <yujiokano@keio.jp>
2023-01-29 20:43:26 +0900

    :sparkles: add test for grnet.dev.kwarg_mgr

--- tests/dev/test_fig.py
+++ tests/dev/test_fig.py
@@ -66,3 +66,59 @@ def test_invalid_typehint_tuple(not_tuple_of_types):
             kwarg_mgr(kwargs={"asdf": 1}, key="asdf", default=None, typehint=v)
         assert "typehint" in f"{e.value}", \
             f"test failed for {i}-th input {v}"
+
+
+def test_not_found():
+    kwargs = [
+        {},
+        {f"a{i}": i for i in range(5)}
+    ]
+    for i, v in enumerate(kwargs):
+        ret = kwarg_mgr(kwargs=v, key="asdf", default=None)
+        assert ret is None, \
+            f"test failed for {i}-th input: expected None, got {ret}"
+
+
+def test_found():
+    kwargs = [
+        {"asdf": "ghjk"},
+        {**{f"a{i}": i for i in range(5)}, "asdf": "ghjk"}
+    ]
+    for i, v in enumerate(kwargs):
+        ret = kwarg_mgr(kwargs=v, key="asdf", default=None)
+        assert ret  == "ghjk", \
+            f"test failed for {i}-th input: expected 'ghjk', got {ret}"
+
+
+def test_not_found_and_typecheck():
+    kwargs = [
+        {},
+        {f"a{i}": i for i in range(5)}
+    ]
+    for i, v in enumerate(kwargs):
+        ret = kwarg_mgr(kwargs=v, key="asdf", default=None, typehint=int)
+        assert ret is None, \
+            f"test failed for {i}-th input: expected None, got {ret}"
+
+
+def test_found_and_invalid_type():
+    kwargs = [
+        {"asdf": "ghjk"},
+        {**{f"a{i}": i for i in range(5)}, "asdf": "ghjk"}
+    ]
+    for i, v in enumerate(kwargs):
+        with pytest.raises(AssertionError) as e:
+            kwarg_mgr(kwargs=v, key="asdf", default=1, typehint=int)
+        assert "asdf" in f"{e.value}", \
+            f"test failed for {i}-th input"
+
+
+def test_found_and_typecheck():
+    kwargs = [
+        {"asdf": "ghjk"},
+        {**{f"a{i}": i for i in range(5)}, "asdf": "ghjk"}
+    ]
+    for i, v in enumerate(kwargs):
+        ret = kwarg_mgr(kwargs=v, key="asdf", default=None, typehint=str)
+        assert ret == "ghjk", \
+            f"test failed for {i}-th input: expected 'ghjk', got {ret}"
