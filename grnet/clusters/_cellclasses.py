360634c3adcce80abc7ea1fc8d8ab54312e2dd4b
yo-aka-gene <yujiokano@keio.jp>
2023-01-30 16:58:44 +0900

    :construction: 23/1/30 16:58

---
+++ grnet/clusters/_cellclasses.py
@@ -0,0 +1,104 @@
+"""
+Class to manage cell classes in in a dataset
+"""
+from typing import Dict, List, Tuple, Union
+
+import matplotlib.pyplot as plt
+
+from grnet.abstract import Estimator
+from grnet.dev import is_grn_matrix, typechecker, valchecker
+
+
+class CellClasses:
+    """
+    Class to manage cell classes in in a dataset
+
+    Methods
+    -------
+    __init__(
+        self,
+        models,
+        names,
+        colors
+    ) -> None:
+        initialize attributes
+
+    fetch(
+        self,
+        id: Union[int, str]
+    ) -> Dict[str, Union[Estimator, str, int, Tuple[float]]]:
+        fetch a set of information about a cell class
+
+    Attributes
+    ----------
+    models: Dict[int, grnet.abstract.Estimator]
+    grns: Dict[int, pandas.core.frame.DataFrame]
+    names: Dict[int, Union[str, int]]
+    colors: Dict[int, Union[str, Tuple[float]]]
+    """
+    def __init__(
+        self,
+        models: List[Estimator],
+        names: List[Union[str, int]] = None,
+        colors: Union[List[Union[Tuple[float], str]], str] = None,
+    ) -> None:
+        """
+        Parameters
+        ----------
+        models: List[grnet.abstract.Estimator]
+        names: List[Union[str, int]], default: None
+        colors: Union[List[Union[Tuple[float], str]], str], default: None
+
+        Returns
+        -------
+        None
+        """
+        typechecker(models, list, "models")
+        for i, v in enumerate(models):
+            typechecker(v, Estimator, f"models[{i}]")
+            is_grn_matrix(v.get_matrix())
+        self.models = {i: v for i, v in enumerate(models)}
+        self.grns = {i: v.get_matrix() for i, v in enumerate(models)}
+        if names is None:
+            self.names = {i: i for i in range(len(models))}
+        else:
+            typechecker(names, list, "names")
+            valchecker(
+                len(names) == len(models),
+                "Length of `names` should be equal to the length of `models`"
+            )
+            for i, v in enumerate(names):
+                typechecker(v, (str, int), f"names[{i}]")
+            self.names = {i: v for i, v in enumerate(names)}
+        if colors is None:
+            self.colors = {
+                i: f"C{i}" for i in range(len(models))
+            } if len(models) <= 10 else {
+                i: plt.cm.jet(i/len(models)) for i in range(len(models))
+            }
+        else:
+            typechecker(colors, (list, str), "colors")
+            if isinstance(colors, list):
+                valchecker(
+                    len(colors) == len(models),
+                    "Length of `names` should be equal to the length of `models`"
+                )
+                for i, v in enumerate(colors):
+                    typechecker(v, (tuple, str), f"colors[{i}]")
+                    if isinstance(v, tuple):
+                        valchecker(len(v) == 4, f"Length of colors[{i}] should be 4")
+                        for i_el, el in enumerate(v):
+                            typechecker(el, float, f"colors[{i}][{i_el}]")
+                        
+                self.colors = {i: v for i, v in enumerate(colors)}
+            else:
+                self.colors = {
+                    i: eval(f"plt.cm.{colors}")(i / len(models)) for i in range(len(models))
+                }
+        return None
+
+    def fetch(
+        self,
+        id: Union[int, str]
+    ) -> Dict[str, Union[Estimator, str, int, Tuple[float]]]:
+        pass
