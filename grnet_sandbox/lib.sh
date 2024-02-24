256385548a4f16b6e83a1ce8cd44224dd6982168
yo-aka-gene <yujiokano@keio.jp>
2024-02-24 17:50:07 +0900

    :bug: fix bug in lib.sh

--- grnet_sandbox/lib.sh
+++ grnet_sandbox/lib.sh
@@ -6,6 +6,7 @@ if [ $USERNAME = "jovyan" ]; then
     pip install --no-deps -r $(dirname $0)/config/requirements.txt
     pip install --no-deps -r $(dirname $0)/config/sandbox_reqs.txt
 else
-    echo "Run in jupyter server"
-    echo $USERNAME
+    VMNAME=$(basename $(dirname $(find . -name Dockerfile)) | tr '[A-Z]' '[a-z]')-jupyterlab-1
+    docker exec --user jovyan $VMNAME pip install --no-deps -r /home/jovyan/config/requirements.txt
+    docker exec --user jovyan $VMNAME pip install --no-deps -r /home/jovyan/config/sandbox_reqs.txt
 fi
