f7ad6c431ac9ee7ba7eb2066083f6fadf823adf9
yo-aka-gene <yujiokano@keio.jp>
2024-02-24 16:53:56 +0900

    :recycle: update writelib.sh

--- grnet_sandbox/writelib.sh
+++ grnet_sandbox/writelib.sh
@@ -5,6 +5,6 @@ USERNAME=$(whoami)
 if [ $USERNAME = "jovyan" ]; then
     pip list --format=freeze > $(dirname $0)/config/sandbox_reqs.txt
 else
-    echo "Run in jupyter server"
-    echo $USERNAME
+    VMNAME=$(basename $(dirname $(find . -name Dockerfile)) | tr '[A-Z]' '[a-z]')-jupyterlab-1
+    docker exec --user jovyan $VMNAME sh /home/jovyan/writelib.sh
 fi
