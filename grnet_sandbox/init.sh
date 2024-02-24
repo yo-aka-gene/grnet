3851332f7e8309f940afa02f3736bd75960c99ae
yo-aka-gene <yujiokano@keio.jp>
2024-02-24 18:10:28 +0900

    :recycle: call lib.sh writelib.sh set_renv.R

--- grnet_sandbox/init.sh
+++ grnet_sandbox/init.sh
@@ -6,4 +6,7 @@ docker-compose up -d
 sh auth.sh docker-compose.yml
 docker compose up -d
 docker start $VMNAME
+docker exec --user jovyan $VMNAME sh /home/jovyan/lib.sh
+docker exec --user jovyan $VMNAME sh /home/jovyan/writelib.sh
+docker exec --user jovyan $VMNAME Rscript /home/jovyan/tools/set_renv.R
 open http://localhost:8000
