a7923c1a738812521c9427bdf327336a3cce583c
yo-aka-gene <yujiokano@keio.jp>
2024-04-02 22:42:41 +0900

    :sparkles::wrench: add datasets for logistic model

---
+++ grnet_sandbox/data/quartzseq2.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+DATA_DIR=/home/jovyan/data
+
+curl -o $DATA_DIR/matrix.tsv.gz https://ftp.ncbi.nlm.nih.gov/geo/series/GSE133nnn/GSE133543/suppl/GSE133543%5FQUARTZseq%5Fhuman%5Fexp%5Fmat.tsv.gz
+curl -o $DATA_DIR/meta.tsv.gz https://ftp.ncbi.nlm.nih.gov/geo/series/GSE133nnn/GSE133543/suppl/GSE133543%5FQUARTZseq%5Fhuman%5Fmetada.tsv.gz
+cd $DATA_DIR; gzip -d *.gz
+python /home/jovyan/data/make_h5ad.py -i "/home/jovyan/data/matrix.tsv" -o "/home/jovyan/data/quartzseq2.h5ad" -f "text" -m "/home/jovyan/data/meta.tsv"
+rm -rf /home/jovyan/data/*.tsv
