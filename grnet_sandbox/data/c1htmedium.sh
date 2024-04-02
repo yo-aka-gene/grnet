a6c70d8f6f9dc1b7a73a6a2945d90174b22e97f4
yo-aka-gene <yujiokano@keio.jp>
2024-04-02 21:54:45 +0900

    :sparkles::wrench: add c1htmedium

---
+++ grnet_sandbox/data/c1htmedium.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+DATA_DIR=/home/jovyan/data
+
+curl -o $DATA_DIR/matrix.tsv.gz https://ftp.ncbi.nlm.nih.gov/geo/series/GSE133nnn/GSE133537/suppl/GSE133537%5FC1HTmedium%5Fhuman%5Fexp%5Fmat.tsv.gz
+curl -o $DATA_DIR/meta.tsv.gz https://ftp.ncbi.nlm.nih.gov/geo/series/GSE133nnn/GSE133537/suppl/GSE133537%5FC1HTmedium%5Fhuman%5Fmetada.tsv.gz
+cd $DATA_DIR; gzip -d *.gz
+python /home/jovyan/data/make_h5ad.py -i "/home/jovyan/data/matrix.tsv" -o "/home/jovyan/data/c1htmedium.h5ad" -f "text" -m "/home/jovyan/data/meta.tsv"
+rm -rf /home/jovyan/data/*.tsv
