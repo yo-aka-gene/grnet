022da24d454f84e4017fb269cd08f3d7adad3fda
yo-aka-gene <yujiokano@keio.jp>
2024-04-03 12:37:21 +0900

    :recycle: correct dataset names

--- grnet_sandbox/data/celseq2.sh
+++ grnet_sandbox/data/celseq2.sh
@@ -5,5 +5,5 @@ DATA_DIR=/home/jovyan/data
 curl -o $DATA_DIR/matrix.tsv.gz https://ftp.ncbi.nlm.nih.gov/geo/series/GSE133nnn/GSE133539/suppl/GSE133539%5FCELseq2%5Fhuman%5Fexp%5Fmat.tsv.gz
 curl -o $DATA_DIR/meta.tsv.gz https://ftp.ncbi.nlm.nih.gov/geo/series/GSE133nnn/GSE133539/suppl/GSE133539%5FCELseq2%5Fhuman%5Fmetada.tsv.gz
 cd $DATA_DIR; gzip -d *.gz
-python /home/jovyan/data/make_h5ad.py -i "/home/jovyan/data/matrix.tsv" -o "/home/jovyan/data/cellseq2.h5ad" -f "text" -m "/home/jovyan/data/meta.tsv"
+python /home/jovyan/data/make_h5ad.py -i "/home/jovyan/data/matrix.tsv" -o "/home/jovyan/data/celseq2.h5ad" -f "text" -m "/home/jovyan/data/meta.tsv"
 rm -rf /home/jovyan/data/*.tsv
--- grnet_sandbox/data/gmcscrbseq.sh
+++ grnet_sandbox/data/gmcscrbseq.sh
@@ -5,5 +5,5 @@ DATA_DIR=/home/jovyan/data
 curl -o $DATA_DIR/matrix.tsv.gz https://ftp.ncbi.nlm.nih.gov/geo/series/GSE133nnn/GSE133544/suppl/GSE133544%5FSCRBseq%5Fhuman%5Fexp%5Fmat.tsv.gz
 curl -o $DATA_DIR/meta.tsv.gz https://ftp.ncbi.nlm.nih.gov/geo/series/GSE133nnn/GSE133544/suppl/GSE133544%5FSCRBseq%5Fhuman%5Fmetada.tsv.gz
 cd $DATA_DIR; gzip -d *.gz
-python /home/jovyan/data/make_h5ad.py -i "/home/jovyan/data/matrix.tsv" -o "/home/jovyan/data/gmscrbseq.h5ad" -f "text" -m "/home/jovyan/data/meta.tsv"
+python /home/jovyan/data/make_h5ad.py -i "/home/jovyan/data/matrix.tsv" -o "/home/jovyan/data/gmcscrbseq.h5ad" -f "text" -m "/home/jovyan/data/meta.tsv"
 rm -rf /home/jovyan/data/*.tsv
