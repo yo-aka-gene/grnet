5c6735afc4f385e3df09e90ca6be5a3dd6ca7087
yo-aka-gene <yujiokano@keio.jp>
2024-05-09 17:07:36 +0900

    :wrench::sparkles: add AlgebraicArrays

--- grnet/dev/__init__.py
+++ grnet/dev/__init__.py
@@ -1,12 +1,13 @@
 from ._cellclass_checker import is_cellclass_dict
 from ._checker import typechecker, valchecker
 from ._fig import kwarg_mgr
-from ._generic_types import ScipySparseMatrix
+from ._generic_types import ScipySparseMatrix, AlgebraicArrays
 from ._grn_checker import is_grn_matrix
 from ._set_operations import multi_union, multi_intersec
 from ._typemolds import typemolds
 
 __all__ = [
+    AlgebraicArrays,
     "is_cellclass_dict",
     "is_grn_matrix",
     "multi_union",
--- grnet/dev/_generic_types.py
+++ grnet/dev/_generic_types.py
@@ -1,9 +1,16 @@
 from typing import Union
 
+import numpy as np
 from scipy.sparse import coo_matrix, csr_matrix, csc_matrix
+from torch import Tensor
 
 ScipySparseMatrix = Union[
     coo_matrix,
     csr_matrix,
     csc_matrix
+]
+
+AlgebraicArrays = Union[
+    np.ndarray,
+    Tensor
 ]
