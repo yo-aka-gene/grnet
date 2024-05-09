033ec4cd062d07ea9eaf22310ce6422c36666e76
yo-aka-gene <yujiokano@keio.jp>
2024-05-09 23:29:38 +0900

    :recycle: adjust to tools.dor.models

--- grnet_sandbox/tools/dor/metrics/_inv_models.py
+++ grnet_sandbox/tools/dor/metrics/_inv_models.py
@@ -1,6 +1,7 @@
+from typing import Callable
+
 import numpy as np
 from sklearn.metrics import mean_absolute_error as mae
-from statsmodels.genmod.generalized_linear_model import GLMResultsWrapper
 
 from grnet.dev import AlgebraicArrays, typechecker
 
@@ -8,39 +9,31 @@ from grnet.dev import AlgebraicArrays, typechecker
 def horizontal_mae(
     x: AlgebraicArrays,
     y: AlgebraicArrays,
-    model: GLMResultsWrapper,
+    inv_model: Callable,
     avoid_inf: bool = True
 ) -> np.ndarray:
     typechecker(x, AlgebraicArrays, "x")
     typechecker(y, AlgebraicArrays, "y")
-    typechecker(model, GLMResultsWrapper, "model")
+    typechecker(inv_model, Callable, "inv_model")
     typechecker(avoid_inf, bool, "avoid_inf")
-    y_intercep, coeff = model.params
-    inv = lambda y: (
-        (np.log(y) - y_intercep) / coeff
-    )
     return mae(
         x[np.where(y != 0)] if avoid_inf else x,
-        np.frompyfunc(inv, 1, 1)(y[np.where(y != 0)] if avoid_inf else y)
+        np.frompyfunc(inv_model, 1, 1)(y[np.where(y != 0)] if avoid_inf else y)
     )
 
 
 def horizontal_maxae(
     x: AlgebraicArrays,
     y: AlgebraicArrays,
-    model: GLMResultsWrapper,
+    inv_model: Callable,
     avoid_inf: bool = True
 ) -> np.ndarray:
     typechecker(x, AlgebraicArrays, "x")
     typechecker(y, AlgebraicArrays, "y")
-    typechecker(model, GLMResultsWrapper, "model")
+    typechecker(inv_model, Callable, "inv_model")
     typechecker(avoid_inf, bool, "avoid_inf")
-    y_intercep, coeff = model.params
-    inv = lambda y: (
-        (np.log(y) - y_intercep) / coeff
-    )
     return np.abs(
-        x[np.where(y != 0)] - np.frompyfunc(inv, 1, 1)(y[np.where(y != 0)])
+        x[np.where(y != 0)] - np.frompyfunc(inv_model, 1, 1)(y[np.where(y != 0)])
     ).max() if avoid_inf else np.abs(
-        x - np.frompyfunc(inv, 1, 1)(y)
+        x - np.frompyfunc(inv_model, 1, 1)(y)
     ).max()
