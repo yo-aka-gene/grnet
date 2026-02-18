ee852649fd3d52a9d10b103a0cc5e0207b73cd6b
yo-aka-gene <yujiokano@keio.jp>
2026-02-18 17:22:31 +0900

    :recycle::wrench: change module path from grnet to src/grnet

--- docs/conf.py
+++ docs/conf.py
@@ -9,72 +9,72 @@
 import glob
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath("../src"))
 
 import grnet
 
-project = 'Grnet'
-copyright = '2023, Yuji Okano'
-author = 'Yuji Okano'
+project = "Grnet"
+copyright = "2023, Yuji Okano"
+author = "Yuji Okano"
 version = grnet.__version__
 release = grnet.__version__
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'nbsphinx', 'sphinx_gallery.load_style', 'myst_parser']
+extensions = [
+    "sphinx.ext.autodoc",
+    "nbsphinx",
+    "sphinx_gallery.load_style",
+    "myst_parser",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-htmlhelp_basename = 'grnet_doc'
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+htmlhelp_basename = "grnet_doc"
 html_logo = "_static/title.PNG"
-html_theme_options = {"navigation_depth": 5, "logo_only": True, "sidebarbgcolor": "#003F67"}
-master_doc = 'index'
+html_theme_options = {
+    "navigation_depth": 5,
+    "logo_only": True,
+    "sidebarbgcolor": "#003F67",
+}
+master_doc = "index"
 latex_documents = [
-    (master_doc, 'grnet.tex',
-     'Grnet Documentation',
-     'Yuji Okano', 'manual'),
+    (master_doc, "grnet.tex", "Grnet Documentation", "Yuji Okano", "manual"),
 ]
 
-man_pages = [
-    (master_doc, 'grnet',
-     'Grnet Documentation',
-     [author], 1)
-]
+man_pages = [(master_doc, "grnet", "Grnet Documentation", [author], 1)]
 
 texinfo_documents = [
-    (master_doc, 'grnet',
-     'Grnet Documentation',
-     author,
-     'grnet',
-     'Python package for gene regulatory networks (GRN) using Bayesian network',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "grnet",
+        "Grnet Documentation",
+        author,
+        "grnet",
+        "Python package for gene regulatory networks (GRN) using Bayesian network",
+        "Miscellaneous",
+    ),
 ]
 
 nbsphinx_thumbnails = {
-    "/".join(
-        v.split(".")[:-1]
-    ): v.replace(
-        "notebooks", "_static"
-    ).replace(
-        "ipynb", "png"
-    ) if os.path.exists(
-        v.replace(
-            "notebooks", "_static"
-        ).replace(
-            "ipynb", "png"
-        )
-    ) else "_static/logo.PNG" for v in glob.glob("notebooks/*")
+    "/".join(v.split(".")[:-1]): (
+        v.replace("notebooks", "_static").replace("ipynb", "png")
+        if os.path.exists(v.replace("notebooks", "_static").replace("ipynb", "png"))
+        else "_static/logo.PNG"
+    )
+    for v in glob.glob("notebooks/*")
 }
 
 source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
+    ".rst": "restructuredtext",
+    ".md": "markdown",
 }
