16e8622c00d9f62bff160f99ea89c4b97d913f04
yo-aka-gene <yujiokano@keio.jp>
2026-02-25 15:59:52 +0900

    :bug: fix bug in unpacking downloaded tar.gz file

--- grnet_sandbox/data/pbmc3k.sh
+++ grnet_sandbox/data/pbmc3k.sh
@@ -4,8 +4,7 @@ DATA_DIR=$(cd "$(dirname "$0")" && pwd)
 SCRIPT_NAME=$(basename "$0" .sh)
 
 wget http://cf.10xgenomics.com/samples/cell-exp/1.1.0/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz -O "$DATA_DIR/$SCRIPT_NAME.tar.gz"
-tar -xzf "$DATA_DIR/$SCRIPT_NAME.tar.gz"
-gzip -d "$DATA_DIR"/*.gz
+tar -xzf "$DATA_DIR/$SCRIPT_NAME.tar.gz" -C "$DATA_DIR"
 python "$DATA_DIR/make_h5ad.py" -i "$DATA_DIR/filtered_gene_bc_matrices/hg19" -o "$DATA_DIR/$SCRIPT_NAME.h5ad"
-rm -f "$DATA_DIR"/*.tar
+rm -f "$DATA_DIR"/*.tar.gz
 rm -rf "$DATA_DIR/filtered_gene_bc_matrices"
