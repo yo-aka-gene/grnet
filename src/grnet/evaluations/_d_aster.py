54debd2795f1616f602ba81699aded1139c2ca24
yo-aka-gene <yujiokano@keio.jp>
2026-03-08 16:37:10 +0900

    :recycle: change arguments pd.DataFrame -> Estimator

--- src/grnet/evaluations/_d_aster.py
+++ src/grnet/evaluations/_d_aster.py
@@ -4,12 +4,11 @@ GRN evaluation function d_asterisk
 
 import pandas as pd
 
+from grnet.abstract import Estimator
 from grnet.dev import is_grn_matrix, typechecker
 
 
-def d_asterisk(
-    subjective: pd.core.frame.DataFrame, objective: pd.core.frame.DataFrame
-) -> float:
+def d_asterisk(subjective: Estimator, objective: Estimator) -> float:
     """
     | GRN evaluation function :math:`d^*` is suitable for undirected GRNs generated with PC algorithm.\\
     When :math:`S`: Set of samples, :math:`\\xi:S\\rightarrow S/\\sim` is\
@@ -26,11 +25,11 @@ def d_asterisk(
 
     Parameters
     ----------
-    subjective: pandas.core.frame.DataFrame
-        GRN matrix of the subjective cluster (i.e., cell class)
+    subjective: Estimator
+        GRNet model of the subjective cluster (i.e., cell class)
 
-    objective: pandas.core.frame.DataFrame
-        GRN matrix of the objective cluster (i.e., cell class)
+    objective: Estimator
+        GRNet model of the objective cluster (i.e., cell class)
 
     Returns
     -------
@@ -47,22 +46,24 @@ def d_asterisk(
     >>> import numpy as np
     >>> import pandas as pd
     >>> from grnet.evaluations import d_asterisk
+    >>> from grnet.models import PretrainedModel
     >>> # here we deal undirected GRNs
     >>> names = [f"gene_{i}" for i in range(4)]
-    >>> cluster1 = pd.DataFrame(np.eye(4), index=names, columns=names)
-    >>> cluster2 = pd.DataFrame(np.tri(4), index=names, columns=names)
-    >>> cluster1
+    >>> grn1 = pd.DataFrame(np.eye(4), index=names, columns=names)
+    >>> grn2 = pd.DataFrame(np.tri(4), index=names, columns=names)
+    >>> grn1
             gene_0  gene_1  gene_2  gene_3
     gene_0     1.0     0.0     0.0     0.0
     gene_1     0.0     1.0     0.0     0.0
     gene_2     0.0     0.0     1.0     0.0
     gene_3     0.0     0.0     0.0     1.0
-    >>> cluster2
+    >>> grn2
             gene_0  gene_1  gene_2  gene_3
     gene_0     1.0     0.0     0.0     0.0
     gene_1     1.0     1.0     0.0     0.0
     gene_2     1.0     1.0     1.0     0.0
     gene_3     1.0     1.0     1.0     1.0
+    >>> cluster1, cluster2 = PretrainedModel(data=grn1), PretrainedModel(data=grn2)
     >>> d_asterisk(cluster1, cluster1)
     0.0
     >>> d_asterisk(cluster1, cluster2)
@@ -72,10 +73,12 @@ def d_asterisk(
     >>> d_asterisk(cluster2, cluster1)
     0.6
     """
-    typechecker(subjective, pd.core.frame.DataFrame, "subjective")
-    typechecker(objective, pd.core.frame.DataFrame, "objective")
-    is_grn_matrix(subjective)
-    is_grn_matrix(objective)
-    s = subjective.loc[subjective.index, subjective.index]
-    o = objective.loc[s.index, s.columns]
+    typechecker(subjective, Estimator, "subjective")
+    typechecker(objective, Estimator, "objective")
+    s_grn = subjective.get_matrix()
+    o_grn = objective.get_matrix()
+    is_grn_matrix(s_grn)
+    is_grn_matrix(o_grn)
+    s = s_grn.loc[s_grn.index, s_grn.index]
+    o = o_grn.loc[s.index, s.columns]
     return 1 - (((s.values == 1) * (o.values == 1)).sum() / (s.values == 1).sum())
--- src/grnet/evaluations/_whqpm.py
+++ src/grnet/evaluations/_whqpm.py
@@ -5,46 +5,39 @@ GRN evaluation function Weighted Hamming Quasi-pseudo Metric
 import numpy as np
 import pandas as pd
 
+from grnet.abstract import Estimator
 from grnet.dev import is_grn_matrix, typechecker
 
 
-def whqpm(
-    subjective: pd.DataFrame,
-    objective: pd.DataFrame,
-    s_rawdata: pd.DataFrame,
-    o_rawdata: pd.DataFrame,
-) -> float:
+def whqpm(subjective: Estimator, objective: Estimator) -> float:
     """
     Parameters
     ----------
-    subjective: pandas.core.frame.DataFrame
-        GRN matrix of the subjective cluster (i.e., cell class)
+    subjective: Estimator
+        GRNet model of the subjective cluster (i.e., cell class)
 
-    objective: pandas.core.frame.DataFrame
-        GRN matrix of the objective cluster (i.e., cell class)
-
-    s_rawdata: pandas.DataFrame
-        NxD matrix (N: number of samples, D: number of genes) of subjective cell class
-
-    o_rawdata: pandas.DataFrame
-        NxD matrix (N: number of samples, D: number of genes) of objective cell class
+    objective: Estimator
+        GRNet model of the objective cluster (i.e., cell class)
 
     Returns
     -------
     Quasi-pseudo distance: float
         quasi-pseudo distance (centering the subjective cluster) between the two clusters
     """
-    typechecker(subjective, pd.DataFrame, "subjective")
-    typechecker(objective, pd.DataFrame, "objective")
-    is_grn_matrix(subjective)
-    is_grn_matrix(objective)
-    typechecker(s_rawdata, pd.DataFrame, "s_rawdata")
-    typechecker(o_rawdata, pd.DataFrame, "o_rawdata")
-    s = subjective.loc[subjective.index, subjective.index]
-    o = objective.loc[s.index, s.columns]
+    typechecker(subjective, Estimator, "subjective")
+    typechecker(objective, Estimator, "objective")
+    s_grn = subjective.get_matrix()
+    s_rawdata = subjective.data
+    o_grn = objective.get_matrix()
+    o_rawdata = objective.data
+    is_grn_matrix(s_grn)
+    is_grn_matrix(o_grn)
+    s = s_grn.loc[s_grn.index, s_grn.index]
+    o = o_grn.loc[s.index, s.columns]
     s_diag = (s_rawdata.loc[:, s.index] != 0).sum() / s_rawdata.shape[0]
     o_diag = (o_rawdata.loc[:, s.index] != 0).sum() / o_rawdata.shape[0]
+    diag_min = pd.concat([s_diag, o_diag], axis=1).min(axis=1)
     s_other = s.values - np.eye(s.shape[0])
     o_other = o.values - np.eye(o.shape[0])
     n_shared_edges = (s_other.astype(bool) & o_other.astype(bool)).sum()
-    return 1 - (n_shared_edges + o_diag.sum()) / (s_other.sum() + s_diag.sum())
+    return 1 - (n_shared_edges + diag_min.sum()) / (s_other.sum() + s_diag.sum())
